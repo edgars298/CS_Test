98d2aaa8a89759068d1c2b354c20b4a97e962ee4
Edgars Šaicāns <62073005+edgars298@users.noreply.github.com>
2025-06-09 09:18:44 +0300

    Initial Create

---
+++ Api/Program.cs
@@ -0,0 +1,33 @@
+using BusinessLogic.Models;
+using BusinessLogic.Services;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var connectionString =
+    builder.Configuration.GetConnectionString("DefaultConnection")
+        ?? throw new InvalidOperationException("Connection string not configured.");
+
+builder.Services.AddDbContext<ApplicationDbContext>(o => o.UseSqlServer(connectionString));
+
+builder.Services.AddScoped<IApplicationDbContext, ApplicationDbContext>(serviceProvider => 
+    serviceProvider.GetService<ApplicationDbContext>() ?? throw new InvalidOperationException("Could not register database context."));
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
+
---
+++ BusinessLogic/Services/BaseService.cs
@@ -0,0 +1,28 @@
+﻿using BusinessLogic.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BusinessLogic.Services;
+
+public class BaseService<T>(IApplicationDbContext context) where T : class
+{
+    protected readonly IApplicationDbContext context = context;
+    protected readonly DbSet<T> dbset = context.Set<T>();
+
+    public virtual void Add(T entity)
+    {
+        dbset.Add(entity);
+        context.SaveChanges();
+    }
+
+    public virtual T? Find(int id)
+    {
+        return dbset.Find(id);
+    }
+}
+
+public interface IBaseService<T> where T : class
+{
+    void Add(T entity);
+
+    T? Find(int id);
+}
